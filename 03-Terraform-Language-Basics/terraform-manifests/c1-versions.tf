3927ec2c5731bc563ee8108c372f86f2317d05fd
phanienliven <phanienliven@outlook.com>
2024-08-29 16:25:17 -0500

    changesrequired

--- 03-Terraform-Language-Basics/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Language-Basics/terraform-manifests/c1-versions.tf
@@ -1,17 +1,17 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.8.5"
+  required_version = ">=1/8.5"
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = ">= 5.33.0"
+      version = ">=5.33.0"
     }
   }
 }
-
+ 
 # Terraform Provider Block
 provider "google" {
-  project = "gcplearn9" # PROJECT_ID
+  project = "euphoric-quanta-433916-q7" # PROJECT_ID
   region = "us-central1"
 }
 
--- 03-Terraform-Language-Basics/terraform-manifests/c3-vpc.tf
+++ 03-Terraform-Language-Basics/terraform-manifests/c3-vpc.tf
@@ -1,13 +1,13 @@
-# Resource: VPC
+# Resource Block : VPC
 resource "google_compute_network" "myvpc" {
   name = "vpc1"
-  auto_create_subnetworks = false   
+  auto_create_subnetworks = false
 }
 
-# Resource: Subnet
-resource "google_compute_subnetwork" "mysubnet" {
-  name = "subnet1"
-  region = "us-central1"
-  ip_cidr_range = "10.128.0.0/20"
-  network = google_compute_network.myvpc.id 
-}
+# Resouce Block : Subnet
+resource "google_compute_subnetwork" "mysubnet123" {
+ name = "mysubnet1"
+ region = "us-central1"
+ ip_cidr_range = "10.128.0.0/20"
+ network = google_compute_network.myvpc.id 
+ }
