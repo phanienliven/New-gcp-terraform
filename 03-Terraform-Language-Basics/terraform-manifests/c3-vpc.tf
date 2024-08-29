0a4be9d5f058f1a9f529800cd889fe64b7c03125
phanienliven <phanienliven@outlook.com>
2024-08-29 16:42:22 -0500

    changes6

--- 03-Terraform-Language-Basics/terraform-manifests/c3-vpc.tf
+++ 03-Terraform-Language-Basics/terraform-manifests/c3-vpc.tf
@@ -1,13 +1,13 @@
-# Resource Block : VPC
+# Resource: VPC
 resource "google_compute_network" "myvpc" {
   name = "vpc1"
-  auto_create_subnetworks = false
+  auto_create_subnetworks = false   
 }
 
-# Resouce Block : Subnet
-resource "google_compute_subnetwork" "mysubnet123" {
- name = "mysubnet1"
- region = "us-central1"
- ip_cidr_range = "10.128.0.0/20"
- network = google_compute_network.mysubnet.id
- }
+# Resource: Subnet
+resource "google_compute_subnetwork" "mysubnet" {
+  name = "subnet1"
+  region = "us-central1"
+  ip_cidr_range = "10.128.0.0/20"
+  network = google_compute_network.myvpc.id 
+}
